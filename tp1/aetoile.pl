60188ac51adfccd8f88f8bb846bbf6106f97e634
José Daniel Organista <josedaniel@agrapp.co>
2022-04-01 18:45:02 +0200

    :tada: first commit

---
+++ tp1/aetoile.pl
@@ -0,0 +1,325 @@
+%*******************************************************************************
+%                                    AETOILE
+%*******************************************************************************
+
+/*
+Rappels sur l'algorithme
+ 
+- structures de donnees principales = 2 ensembles : P (etat pendants) et Q (etats clos)
+- P est dedouble en 2 arbres binaires de recherche equilibres (AVL) : Pf et Pu
+ 
+   Pf est l'ensemble des etats pendants (pending states), ordonnes selon
+   f croissante (h croissante en cas d'egalite de f). Il permet de trouver
+   rapidement le prochain etat a developper (celui qui a f(U) minimum).
+   
+   Pu est le meme ensemble mais ordonne lexicographiquement (selon la donnee de
+   l'etat). Il permet de retrouver facilement n'importe quel etat pendant
+
+   On gere les 2 ensembles de fa�on synchronisee : chaque fois qu'on modifie
+   (ajout ou retrait d'un etat dans Pf) on fait la meme chose dans Pu.
+
+   Q est l'ensemble des etats deja developpes. Comme Pu, il permet de retrouver
+   facilement un etat par la donnee de sa situation.
+   Q est modelise par un seul arbre binaire de recherche equilibre.
+
+Predicat principal de l'algorithme :
+
+   aetoile(Pf,Pu,Q)
+
+   - reussit si Pf est vide ou bien contient un etat minimum terminal
+   - sinon on prend un etat minimum U, on genere chaque successeur S et les valeurs g(S) et h(S)
+	 et pour chacun
+		si S appartient a Q, on l'oublie
+		si S appartient a Ps (etat deja rencontre), on compare
+			g(S)+h(S) avec la valeur deja calculee pour f(S)
+			si g(S)+h(S) < f(S) on reclasse S dans Pf avec les nouvelles valeurs
+				g et f 
+			sinon on ne touche pas a Pf
+		si S est entierement nouveau on l'insere dans Pf et dans Ps
+	- appelle recursivement etoile avec les nouvelles valeurs NewPF, NewPs, NewQs
+
+*/
+
+%*******************************************************************************
+
+:- ['avl.pl'].       % predicats pour gerer des arbres bin. de recherche   
+:- ['taquin.pl'].    % predicats definissant le systeme a etudier
+
+%*******************************************************************************
+
+main :-
+	% situation de départ
+	initial_state(S0),
+	% calcul des valeurs F0, H0, G0
+	heuristique(S0, H0),
+	G0 is 0,
+	F0 is G0 + H0,
+	% initialisations Pf, Pu et Q 
+	empty(Pf),
+	empty(Pu),
+	empty(Q),
+	% insertion of initial nodes dan Pf, Pu
+	insert([[F0,H0,G0], S0], Pf, Pf1),
+	insert([S0, [F0,H0,G0], nil, nil], Pu, Pu1),
+	put_flat(Pf1),
+	put_flat(Pu1),
+	put_flat(Q),
+	% lancement de Aetoile
+	aetoile(Pf1, Pu1, Q).
+	
+%*******************************************************************************
+
+% format :   expand(+[Current_State, ?[F, H, G], +State_Parent, Action], ?List)
+expand([U, [F, H, G], Pere, Action], L) :-  
+	findall([S, [Fs, Hs, Gs], U, Action], 
+	(rule(Action, Cost, U, S),
+	heuristique(S, Hs),
+	Gs is G + Cost,
+	Fs is Hs + Gs),
+	L).
+	
+/*traitement_successors(Head, Pf, Pf, Pu, Pu, Q) :-
+	Head = [S, [Fs, Hs, Gs], U, Action],
+	belongs(S, Q).
+	
+traitement_successors(Node, Pf, Pf1, Pu, Pu1, Q) :-
+	Node  = [S, Val, Parent, Action],
+	Node1 = [S, Val1, Parent1, Action1],
+	NodePf  = [Val,S],
+	NodePf1 = [Val1,S],
+	suppress(Node1, Pu, Pu1),
+	suppress_min(Node1, Pf, Pf1),
+	Val@<Val1,
+	insert(Node, Pu, Pu1),
+	insert(NodePf, Pf, Pf1).*/
+	
+traitement_successors([S, Val, _, _], Pf, Pf1, Pu, Pu1, Q, Q1) :-
+	suppress_min([Val, S], Pf, Pf1),
+	suppress([S, Val, _, _], Pu, Pu1), 
+	insert([S, Val, _, _], Pu, Pu1),
+	insert([Val, S], Pf, Pf1).
+	
+loop_successors([], Pf, Pf, Pu, Pu, Q).
+	
+%si S est connu dans Q alors oublier cet état (S a déjà été développé)
+loop_successors([[S,_,_,_]|Tail], Pf, Pfn, Pu, Pun, Q) :-
+	belongs([S,_,_,_], Q), 
+	nl,writeln("S est connu dans Q State:"),
+	write_state(S),
+	loop_successors(Tail, Pf, Pfn, Pu, Pun, Q).
+	
+%si S est connu dans Pu alors garder le terme associé à la meilleure évaluation
+%S'il y a une evaluation superiur a le heuristique déjà dévélopé
+loop_successors([[S,Val,_,_]|Tail], Pf, Pfn, Pu, Pun, Q) :-
+	\+belongs([S,_,_,_], Q),
+	belongs([S,Val1,_,_], Pu),
+	nl,writeln("S  est connu dans Pu State:"),
+	write_state(S),
+	nl,writeln("Val: y a une evaluation inferieur a le heuristique déjà dévélopé"),
+	write_state(Val),
+	nl,writeln("Val1:"),
+	write_state(Val1),
+	( compare_values(Val, Val1) ->
+	suppress_min([Val1, S], Pf, Pf1),
+	suppress([S, Val1, _, _], Pu, Pu1), 
+	insert([Val, S], Pf1, Pf2),
+	insert([S, Val, _, _], Pu1, Pu2),	
+	loop_successor(Tail, Pf2, Pfn, Pu2, Pun, Q1)
+	;
+	loop_successor(Tail, Pf, Pfn, Pu, Pun, Q1)
+	).
+	
+%si S est connu dans Pu alors garder le terme associé à la meilleure évaluation
+%S'il y a une evaluation inferieur a le heuristique déjà dévélopé
+/*loop_successors([[S,Val,_,_]|Tail], Pf, Pfn, Pu, Pun, Q) :-
+	\+belongs([S,_,_,_], Q),
+	belongs([S,Val1,_,_], Pu),
+	Val @< Val1,
+	nl,writeln("S  est connu dans Pu State:"),
+	write_state(S),
+	nl,writeln("Val: y a une evaluation inferieur a le heuristique déjà dévélopé"),
+	write_state(Val),
+	nl,writeln("Val1:"),
+	write_state(Val1),
+	suppress_min([Val1, S], Pf, Pf1),
+	suppress([S, Val1, _, _], Pu, Pu1), 
+	insert([Val, S], Pf1, Pf2),
+	insert([S, Val, _, _], Pu1, Pu2),	
+	loop_successor(Tail, Pf2, Pfn, Pu2, Pun, Q1).
+	*/
+	
+%S est une situation nouvelle
+loop_successors([[S, [F, H, G], Pere, Action]|Tail], Pf, Pfn, Pu, Pun, Q) :-
+	\+belongs([S,_,_,_], Q),
+	\+belongs([S,_,_,_], Pu),
+	insert([[F,H,G], S], Pf, Pf1),
+	insert([S, [F, H, G], Pere, Action], Pu, Pu1),
+	writeln("insertion of new S"),
+	writeln("State:"),
+	write_state(S),
+	nl, writeln("Pf1"),
+	put_flat(Pf1),
+	nl, writeln("Pu1"),
+	put_flat(Pu1),
+	nl, writeln("Q"),
+	put_flat(Q),
+	loop_successors(Tail, Pf1, Pfn, Pu1, Pun, Q).
+	
+compare_values(Val, Val1) :- 
+	Val @< Val1.
+
+affiche_solution([U,[F,H,G],Pere, A], Qs) :-
+	belongs([Pere,V,P,A], Qs), 
+	affiche_actions([Pere,V,P,A], Qs),
+	write_state(Pere),
+	writeln("Action:"),
+	writeln(A).
+	
+affiche_solution([U,[F,H,G],nil, nil]) :- writeln("solution trouve: ").
+
+aetoile(nil, nil, Q) :- writeln(' PAS de SOLUTION : L’ETAT FINAL N’EST PAS ATTEIGNABLE !').
+
+aetoile(Pf, Pu, Q) :- 
+	suppress_min([Val, Fin], Pf, _),
+	suppress([Fin, Val, Pere, Action], Pu, _), 
+	final_state(Fin),
+	writeln("Fin state trouve: "),
+	!,
+	affiche_solution([Fin, Val, Pere, Action], Q).
+
+aetoile(Pf, Pu, Q) :-
+	suppress_min([Val, U], Pf, Pf1),
+	suppress([U, Val, Parent, Move], Pu, Pu1), 
+	expand([U, Val, _, _],L),
+	nl,writeln("List Succeseurs"),
+	write_state(L),
+	loop_successors(L, Pf1, Pfn, Pu1, Pun, Q),
+	insert([U, Val, Parent, Move], Q, Qn),
+	nl,writeln("insertion of State en Q"),
+	nl,writeln("Pfn"),
+	put_flat(Pfn),
+	nl,writeln("Pun"),
+	put_flat(Pun),
+	nl,writeln("Qnnnnnnnnnnnnnnnn"),
+	put_flat(Qn),
+	nl,writeln("Qnnnnnnnnnnnnnnnn"),
+	aetoile(Pfn, Pun, Qn).
+	
+test :- 
+	% situation de départ
+	initial_state(S0),
+	% calcul des valeurs F0, H0, G0
+	heuristique(S0, H0),
+	G0 is 0,
+	F0 is G0 + H0,
+	% initialisations Pf, Pu et Q 
+	empty(Pf),
+	empty(Pu),
+	empty(Q),
+	% insertion of initial nodes dan Pf, Pu
+	insert([[F0,H0,G0], S0], Pf, Pf1),
+	insert([S0, [F0,H0,G0], nil, nil], Pu, Pu1),
+	put_flat(Pf1),
+	put_flat(Pu1),
+	writeln("supression Pf min"),
+	suppress_min([Val, U], Pf1, Pfn),
+	writeln("State:"),
+	write_state(U),
+	put_flat(Pfn),
+	writeln("supression Pu min"),
+	suppress([U, Val, _, _], Pu1, Pun), 
+	writeln("State:"),
+	write_state(U),
+	expand([U, Val, _, _],L),
+	writeln("List Succeseurs"),
+	write_state(L).
+	
+test1 :-
+	% situation de départ
+	initial_state(S0),
+	% calcul des valeurs F0, H0, G0
+	heuristique(S0, H0),
+	G0 is 0,
+	F0 is G0 + H0,
+	% initialisations Pf, Pu et Q 
+	empty(Pf),
+	empty(Pu),
+	empty(Q),
+	% insertion some states
+	insert([[F0,H0,G0], S0], Pf, Pf1),
+	insert([S0, [F0,H0,G0], nil, nil], Pu, Pu1),
+	suppress_min([Val, U], Pf1, Pf2),
+	suppress([U, Val, _, _], Pu1, Pu2), 
+	expand([U, Val, _, _],L),
+	writeln("List Succeseurs"),
+	write_state(L),
+	insert([U, Val, Pere, Action], Q, Qs),
+	loop_successors(L, Pf2, Pfn, Pu2, Pun, Qs),	
+	writeln("Pfn"),
+	put_flat(Pfn),
+	writeln("Pun"),
+	put_flat(Pun),
+	writeln("Qs"),
+	put_flat(Qs).
+	
+	
+test2 :-
+	empty(Pf),
+	empty(Pu),
+	empty(Q),
+	initial_state(S0),
+	insert([_, S0], Pf, Pf1),
+	insert([S0, _, nil, nil], Pu, Pu1),
+	insert([S0, _, _, _], Q, Qs),
+	loop_successors([[S0, _, nil, nil]], Pf2, Pfn, Pu2, Pun, Qs).
+	
+test3 :- 
+	% situation de départ
+	initial_state(S0),
+	% calcul des valeurs F0, H0, G0
+	heuristique(S0, H0),
+	G0 is 0,
+	F0 is G0 + H0,
+	% initialisations Pf, Pu et Q 
+	empty(Pf),
+	empty(Pu),
+	empty(Q),
+	% insertion some states
+	insert([[F0,H0,G0], S0], Pf, Pf1),
+	insert([S0, [F0,H0,G0], nil, nil], Pu, Pu1),
+	suppress_min([Val, U], Pf1, Pf2),
+	suppress([U, Val, _, _], Pu1, Pu2), 
+	expand([U, Val, _, _],L),
+	writeln("List Succeseurs"),
+	write_state(L),
+	loop_successors(L, Pf2, Pfn, Pu2, Pun, Q).
+	
+test4 :- 
+	% situation de départ
+	initial_state(S0),
+	% calcul des valeurs F0, H0, G0
+	heuristique(S0, H0),
+	G0 is 0,
+	F0 is G0 + H0,
+	% initialisations Pf, Pu et Q 
+	empty(Pf),
+	empty(Pu),
+	empty(Q),
+	% insertion some states
+	insert([[F0,H0,G0], S0], Pf, Pf1),
+	insert([S0, [F0,H0,G0], nil, nil], Pu, Pu1),
+	suppress_min([Val, U], Pf1, Pf2),
+	suppress([U, Val, Parent, Move], Pu1, Pu2), 
+	expand([U, Val, _, _],L),
+	writeln("List Succeseurs"),
+	write_state(L),
+	loop_successors(L, Pf2, Pf3, Pu2, Pu3, Q),
+	writeln("AAAAAAAAAAAA"),
+	put_flat(Pf3),
+	insert([U, Val, Parent, Move], Q, Q1),
+	suppress_min([Val1, U1], Pf3, Pf4),	
+	suppress([U1, Val1, _, _], Pu3, Pu4), 
+	writeln("jejejejeje"),
+	expand([U1, Val1, _, _],L1),
+	writeln("List Succeseurs 2"),
+	write_state(L1).
---
+++ tp2/tictactoe.pl
@@ -0,0 +1,217 @@
+	/*********************************
+	DESCRIPTION DU JEU DU TIC-TAC-TOE
+	*********************************/
+
+	/*
+	Une situation est decrite par une matrice 3x3.
+	Chaque case est soit un emplacement libre (Variable LIBRE), soit contient le symbole d'un des 2 joueurs (o ou x)
+
+	Contrairement a la convention du tp precedent, pour modeliser une case libre
+	dans une matrice on n'utilise pas une constante speciale (ex : nil, 'vide', 'libre','inoccupee' ...);
+	On utilise plut�t un identificateur de variable, qui n'est pas unifiee (ex : X, A, ... ou _) .
+	La situation initiale est une "matrice" 3x3 (liste de 3 listes de 3 termes chacune)
+	o� chaque terme est une variable libre.	
+	Chaque coup d'un des 2 joureurs consiste a donner une valeur (symbole x ou o) a une case libre de la grille
+	et non a deplacer des symboles deja presents sur la grille.		
+	
+	Pour placer un symbole dans une grille S1, il suffit d'unifier une des variables encore libres de la matrice S1,
+	soit en ecrivant directement Case=o ou Case=x, ou bien en accedant a cette case avec les predicats member, nth1, ...
+	La grille S1 a change d'etat, mais on n'a pas besoin de 2 arguments representant la grille avant et apres le coup,
+	un seul suffit.
+	Ainsi si on joue un coup en S, S perd une variable libre, mais peut continuer a s'appeler S (on n'a pas besoin de la designer
+	par un nouvel identificateur).
+	*/
+
+situation_initiale([ [_,_,_],
+                     [_,_,_],
+                     [_,_,_] ]).
+
+	% Convention (arbitraire) : c'est x qui commence
+
+joueur_initial(x).
+
+
+	% Definition de la relation adversaire/2
+
+adversaire(x,o).
+adversaire(o,x).
+
+
+	/****************************************************
+	 DEFINIR ICI a l'aide du predicat ground/1 comment
+	 reconnaitre une situation terminale dans laquelle il
+	 n'y a aucun emplacement libre : aucun joueur ne peut
+	 continuer a jouer (quel qu'il soit).
+	 ****************************************************/
+
+% situation_terminale(_Joueur, Situation) :-   ? ? ? ? ?
+
+	/***************************
+	DEFINITIONS D'UN ALIGNEMENT
+	***************************/
+
+alignement(L, Matrix) :- ligne(    L,Matrix).
+alignement(C, Matrix) :- colonne(  C,Matrix).
+alignement(D, Matrix) :- diagonale(D,Matrix).
+
+	/********************************************
+	 DEFINIR ICI chaque type d'alignement maximal 
+ 	 existant dans une matrice carree NxN.
+	 ********************************************/
+	
+ligne(L, M) :- nth1(_, M, L).
+
+colonne(C,M) :-
+	maplist(nth0(_), M, C).
+
+	/* Definition de la relation liant une diagonale D a la matrice M dans laquelle elle se trouve.
+		il y en a 2 sortes de diagonales dans une matrice carree(https://fr.wikipedia.org/wiki/Diagonale) :
+		- la premiere diagonale (principale)  : (A I)
+		- la seconde diagonale                : (Z R)
+		A . . . . . . . Z
+		. \ . . . . . / .
+		. . \ . . . / . .
+		. . . \ . / . . .
+		. . . . X . . .
+		. . . / . \ . . . 
+		. . / . . . \ . .
+		. / . . . . . \ .
+		R . . . . . . . I
+	*/
+		
+diagonale(D, M) :- 
+	premiere_diag(1,D,M).
+	
+diagonale(D, M) :- 
+	seconde_diag(3,D,M).
+	
+premiere_diag(_,[],[]).
+premiere_diag(K,[E|D],[Ligne|M]) :-
+	nth1(K,Ligne,E),
+	K1 is K+1,
+	premiere_diag(K1,D,M).
+
+seconde_diag(_,[],[]).
+seconde_diag(K,[E|D],[Ligne|M]) :- 
+	nth1(K,Ligne,E),
+	K1 is K-1,
+	seconde_diag(K1,D,M).
+
+
+	/*****************************
+	 DEFINITION D'UN ALIGNEMENT 
+	 POSSIBLE POUR UN JOUEUR DONNE
+	 *****************************/
+	 
+%A=[_,_,_], possible(A,x).
+
+possible([X|L], J) :- unifiable(X,J), possible(L,J).
+possible(  [],  _).
+
+	/* Attention 
+	il faut juste verifier le caractere unifiable
+	de chaque emplacement de la liste, mais il ne
+	faut pas realiser l'unification.
+	*/
+
+unifiable(X,_) :- var(X).
+unifiable(X,J) :- X==J.
+	
+	/**********************************
+	 DEFINITION D'UN ALIGNEMENT GAGNANT
+	 OU PERDANT POUR UN JOUEUR DONNE J
+	 **********************************/
+	/*
+	Un alignement gagnant pour J est un alignement
+	possible pour J qui n'a aucun element encore libre.
+	*/
+	
+	/*
+	Remarque : le predicat ground(X) permet de verifier qu'un terme
+	prolog quelconque ne contient aucune partie variable (libre).
+	exemples :
+		?- ground(Var).
+		no
+		?- ground([1,2]).
+		yes
+		?- ground(toto(nil)).
+		yes
+		?- ground( [1, toto(nil), foo(a,B,c)] ).
+		no
+	*/
+		
+	/* Un alignement perdant pour J est un alignement gagnant pour son adversaire. */
+
+alignement_gagnant(Ali, J) :- possible(Ali, J), ground(Ali).
+
+alignement_perdant(Ali, J) :- adversaire(J, J1), alignement_gagnant(Ali, J1).
+
+	/* ****************************
+	DEFINITION D'UN ETAT SUCCESSEUR
+	****************************** */
+
+	/* 
+	Il faut definir quelle operation subit la matrice
+	M representant l'Etat courant
+	lorsqu'un joueur J joue en coordonnees [L,C]
+	*/	
+
+successeur(J, Etat,[L,C]) :-  
+	nth1(L,Etat,Lig), 
+	nth1(C,Lig,X),
+	var(X),
+	X = J.
+
+	/**************************************
+   	 EVALUATION HEURISTIQUE D'UNE SITUATION
+  	 **************************************/
+
+	/*
+	1/ l'heuristique est +infini si la situation J est gagnante pour J
+	2/ l'heuristique est -infini si la situation J est perdante pour J
+	3/ sinon, on fait la difference entre :
+	   le nombre d'alignements possibles pour J
+	moins
+ 	   le nombre d'alignements possibles pour l'adversaire de J
+*/
+
+
+heuristique(J,Situation,H) :-		% cas 1
+   H = 10000,				% grand nombre approximant +infini
+   alignement(Alig,Situation),
+   alignement_gagnant(Alig,J), !.
+	
+heuristique(J,Situation,H) :-		% cas 2
+   H = -10000,				% grand nombre approximant -infini
+   alignement(Alig,Situation),
+   alignement_perdant(Alig,J), !.	
+
+
+% on ne vient ici que si les cut precedents n'ont pas fonctionne,
+% c-a-d si Situation n'est ni perdante ni gagnante.
+
+					% cas 3
+heuristique(J,Situation,H) :- 
+	adversaire(J,J1),
+	findall(_,(alignement(Ali, Situation), possible(Ali, J)), L),
+	findall(_,(alignement(Ali, Situation), possible(Ali, J1)), L1),
+	length(L,LenJ),
+	length(L1,LenJ1),
+	H is LenJ - LenJ1.
+	
+% test unitaires
+partie1([ [x,o,o],
+          [o,x,o],
+          [o,o,x] ]).
+	
+partie2([ [x,_,_],
+          [o,_,_],
+          [x,_,_] ]).
+	
+test1 :- partie1(M), alignement(Ali, M), alignement_gagnant(Ali, x).
+test2 :- partie1(M), alignement(Ali, M), alignement_gagnant(Ali, o).
+test3 :- partie1(M), alignement(Ali, M), alignement_perdant(Ali, o).
+test4 :- partie1(M), alignement(Ali, M), alignement_perdant(Ali, x).
+test5 :- situation_initiale(M), heuristique(x,M,H), writeln(H).
+test6 :- partie2(M), heuristique(x,M,H), writeln(H).
+
